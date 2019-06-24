ce14ff79bd6e90c977cfe6632e923da51f73e45f
Pulimet <korolev2009@gmail.com>
2019-06-24 20:39:15 +0300

    MainActivity - Inject MainViewModel and add coroutines scopes

--- app/src/main/java/net/alexandroid/template2019/ui/main/MainActivity.kt
+++ app/src/main/java/net/alexandroid/template2019/ui/main/MainActivity.kt
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import net.alexandroid.template2019.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val job = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + job)
+    private val bgScope = CoroutineScope(Dispatchers.IO + job)
+
+    val mainViewModel: MainViewModel by viewModel()
     private var navController: NavController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +24,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.nav_host_fragment)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
