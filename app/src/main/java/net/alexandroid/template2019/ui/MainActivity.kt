e4d964d3b4fee2aa60ecce9187107379596171ba
Pulimet <korolev2009@gmail.com>
2019-06-24 18:57:07 +0300

    Add Navigation Component and HomeFragment

---
+++ app/src/main/java/net/alexandroid/template2019/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package net.alexandroid.template2019.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import net.alexandroid.template2019.R
+
+class MainActivity : AppCompatActivity() {
+
+    private var navController: NavController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+    }
+}
