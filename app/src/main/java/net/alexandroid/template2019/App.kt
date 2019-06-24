6165c19eacfda2d02422a12c8804edc255131689
Pulimet <korolev2009@gmail.com>
2019-06-24 18:35:14 +0300

    Add and init Logger and Shared Preference helper libraries

--- app/src/main/java/net/alexandroid/template2019/App.kt
+++ app/src/main/java/net/alexandroid/template2019/App.kt
@@ -1,9 +1,16 @@
 package net.alexandroid.template2019
 
 import android.app.Application
+import net.alexandroid.shpref.ShPref
+import net.alexandroid.utils.mylog.MyLog
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        MyLog.init(applicationContext, "Template2019", BuildConfig.DEBUG)
+        MyLog.e("=== Application onCreate()")
+        ShPref.init(applicationContext, ShPref.APPLY)
+
+
     }
 }
