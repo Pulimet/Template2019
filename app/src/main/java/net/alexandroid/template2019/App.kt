ae44ed7b4a7412dfb1753463cdc97a0fb00929c4
Pulimet <korolev2009@gmail.com>
2019-06-24 19:08:11 +0300

    Add Coroutines dependencies + App implements CoroutineScope

--- app/src/main/java/net/alexandroid/template2019/App.kt
+++ app/src/main/java/net/alexandroid/template2019/App.kt
@@ -1,16 +1,20 @@
 package net.alexandroid.template2019
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import net.alexandroid.shpref.ShPref
 import net.alexandroid.utils.mylog.MyLog
+import kotlin.coroutines.CoroutineContext
 
-class App : Application() {
+class App : Application(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
+
     override fun onCreate() {
         super.onCreate()
         MyLog.init(applicationContext, "Template2019", BuildConfig.DEBUG)
         MyLog.e("=== Application onCreate()")
         ShPref.init(applicationContext, ShPref.APPLY)
-
-
     }
 }
