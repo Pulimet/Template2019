7a6ea2332cf2af2c9cc0bb36c1095a6fc8deba22
Alexey Korolev <korolev2009@gmail.com>
2019-06-25 07:41:15 +0300

    Add Retry class + and getDiscoveredMovies to MainViewModel

---
+++ app/src/main/java/net/alexandroid/template2019/network/Retry.kt
@@ -0,0 +1,34 @@
+package net.alexandroid.template2019.network
+
+import kotlinx.coroutines.delay
+import net.alexandroid.utils.mylog.MyLog
+import retrofit2.HttpException
+import java.io.IOException
+
+/**
+ * Created on 1/9/2019 by Alexey Korolev.
+ */
+suspend fun <T> retryIO(
+    times: Int = 4,
+    initialDelay: Long = 1000, // 1 second
+    maxDelay: Long = 4000,    // 4 seconds
+    factor: Double = 2.0,
+    desc: String = "",
+    block: suspend () -> T?
+): T? {
+    var currentDelay = initialDelay
+    repeat(times) {
+        try {
+            return block()
+        } catch (e: IOException) {
+            MyLog.e("Description (IOException): $desc, fail counter: ${it + 1}, Exception: ${e.message} ")
+        } catch (e: HttpException) {
+            MyLog.e("Description (HttpException): $desc, fail counter: ${it + 1}, Exception: ${e.message} ")
+        }
+        delay(currentDelay)
+        currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelay)
+    }
+    return null
+}
+
+
--- app/src/main/java/net/alexandroid/template2019/ui/main/MainViewModel.kt
+++ app/src/main/java/net/alexandroid/template2019/ui/main/MainViewModel.kt
@@ -1,7 +1,24 @@
 package net.alexandroid.template2019.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.launch
+import net.alexandroid.template2019.model.Tmdb
+import net.alexandroid.template2019.network.retryIO
 import net.alexandroid.template2019.ui.base.BaseViewModel
 
 class MainViewModel(private val repo: MainRepository) : BaseViewModel() {
 
+    private lateinit var discoverMovies: MutableLiveData<Tmdb.Discover>
+
+    fun getDiscoveredMovies(): LiveData<Tmdb.Discover> {
+        if (!::discoverMovies.isInitialized) {
+            discoverMovies = MutableLiveData()
+            launch {
+                val result = retryIO(desc = "Discover Movies") { repo.discoverMoviesAsync().await() }
+                if (result != null) discoverMovies.postValue(result)
+            }
+        }
+        return discoverMovies
+    }
 }
