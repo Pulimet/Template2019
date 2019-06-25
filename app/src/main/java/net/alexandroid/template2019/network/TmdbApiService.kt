fa6298e1b2448ef54ca75931d80e242bf7817be4
Alexey Korolev <korolev2009@gmail.com>
2019-06-25 08:21:45 +0300

    TmdbApiService - Ads Query param today date

--- app/src/main/java/net/alexandroid/template2019/network/TmdbApiService.kt
+++ app/src/main/java/net/alexandroid/template2019/network/TmdbApiService.kt
@@ -1,6 +1,7 @@
 package net.alexandroid.template2019.network
 
 import kotlinx.coroutines.Deferred
+import net.alexandroid.template2019.TMDB_API_KEY
 import net.alexandroid.template2019.model.Tmdb
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,7 +9,8 @@ import retrofit2.http.Query
 interface TmdbApiService {
     @GET("3/discover/movie")
     fun discoverAsync(
-        @Query("api_key") apiKey: String,
-        @Query("sort_by") sortBy: String
+        @Query("api_key") apiKey: String = TMDB_API_KEY,
+        @Query("sort_by") sortBy: String = "release_date.desc",
+        @Query("release_date.lte") todayDate: String = "2019-06-25"
     ): Deferred<Tmdb.Discover>
 }
--- app/src/main/java/net/alexandroid/template2019/ui/main/MainRepositoryImpl.kt
+++ app/src/main/java/net/alexandroid/template2019/ui/main/MainRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package net.alexandroid.template2019.ui.main
 
 import kotlinx.coroutines.Deferred
-import net.alexandroid.template2019.TMDB_API_KEY
 import net.alexandroid.template2019.model.Tmdb
 import net.alexandroid.template2019.network.TmdbApiService
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainRepositoryImpl(private val tmdbApiService: TmdbApiService) : MainRepository {
     override fun discoverMoviesAsync(): Deferred<Tmdb.Discover> =
-        tmdbApiService.discoverAsync(TMDB_API_KEY, "release_date.desc")
+        tmdbApiService.discoverAsync(todayDate = getTodayDate())
+
+    private fun getTodayDate(): String = SimpleDateFormat("yyyy-MM-dd", Locale.US).format(Date())
 }
