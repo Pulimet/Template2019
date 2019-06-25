1af6039e313155814259d0c55fc296b6b52add7b
Alexey Korolev <korolev2009@gmail.com>
2019-06-25 08:03:47 +0300

    TmdbApiService - Pass Query params (Api + Sort By)

--- app/src/main/java/net/alexandroid/template2019/network/TmdbApiService.kt
+++ app/src/main/java/net/alexandroid/template2019/network/TmdbApiService.kt
@@ -1,11 +1,14 @@
 package net.alexandroid.template2019.network
 
 import kotlinx.coroutines.Deferred
-import net.alexandroid.template2019.TMDB_API_KEY
 import net.alexandroid.template2019.model.Tmdb
 import retrofit2.http.GET
+import retrofit2.http.Query
 
-interface TmdbApiService{
-    @GET("3/discover/movie?api_key=$TMDB_API_KEY")
-    fun discoverAsync() : Deferred<Tmdb.Discover>
+interface TmdbApiService {
+    @GET("3/discover/movie")
+    fun discoverAsync(
+        @Query("api_key") apiKey: String,
+        @Query("sort_by") sortBy: String
+    ): Deferred<Tmdb.Discover>
 }
--- app/src/main/java/net/alexandroid/template2019/ui/main/MainRepositoryImpl.kt
+++ app/src/main/java/net/alexandroid/template2019/ui/main/MainRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package net.alexandroid.template2019.ui.main
 
 import kotlinx.coroutines.Deferred
+import net.alexandroid.template2019.TMDB_API_KEY
 import net.alexandroid.template2019.model.Tmdb
 import net.alexandroid.template2019.network.TmdbApiService
 
 class MainRepositoryImpl(private val tmdbApiService: TmdbApiService) : MainRepository {
-    override fun discoverMoviesAsync(): Deferred<Tmdb.Discover> = tmdbApiService.discoverAsync()
+    override fun discoverMoviesAsync(): Deferred<Tmdb.Discover> =
+        tmdbApiService.discoverAsync(TMDB_API_KEY, "release_date.desc")
 }
