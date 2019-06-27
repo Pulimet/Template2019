f34a45eca67f0f79291484adbc99db3a15ada28c
Alexey Korolev <korolev2009@gmail.com>
2019-06-27 13:08:11 +0300

    MovieFragment - on back animation

--- app/src/main/java/net/alexandroid/template2019/ui/movie/MovieFragment.kt
+++ app/src/main/java/net/alexandroid/template2019/ui/movie/MovieFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionInflater
 import kotlinx.android.synthetic.main.fragment_movie.*
 import net.alexandroid.template2019.R
@@ -14,13 +17,19 @@ import net.alexandroid.utils.mylog.MyLog
 
 class MovieFragment : BaseFragment() {
     private lateinit var movie: Tmdb.Movie
+    private var isGoBackCalled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+        sharedElementEnterTransition =
+            TransitionInflater.from(context).inflateTransition(android.R.transition.move)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_movie, container, false)
     }
 
@@ -34,5 +43,29 @@ class MovieFragment : BaseFragment() {
         tvTitle.text = movie.getTitleWithYear()
         tvDescription.text = movie.overview
         tvRating.text = String.format("Rating: %s", movie.vote.toString())
+
+        setOnBackPressedListener()
     }
+
+    private fun setOnBackPressedListener() {
+        requireActivity().onBackPressedDispatcher.addCallback {
+            imgMoviePoster.animate()
+                .translationY(0.3f)
+                .scaleX(0.3f)
+                .scaleY(0.3f)
+                .setDuration(500)
+                .setInterpolator(DecelerateInterpolator())
+                .setUpdateListener { goBack() }
+                .start()
+        }
+    }
+
+    private fun goBack() {
+        if (!isGoBackCalled) {
+            isGoBackCalled = true
+            findNavController().popBackStack()
+        }
+    }
+
+
 }
